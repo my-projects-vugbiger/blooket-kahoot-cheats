7d255bb2df540b34a4eafc8acebbe8cf8bdde023
rchambers <158320057+vugbigeriuerr3@users.noreply.github.com>
2025-12-09 19:10:14 +0000

    Push from codespace: 2025-12-09 19:10:14

--- Blooket cheat/cheats/KGui.js
+++ Blooket cheat/cheats/KGui.js
@@ -1,26 +1,3 @@
-/**
- * @license AGPL-3.0
- * Blooket Cheats (Modified Version)
- * Copyright (C) 2023-present 05Konz
- * Copyright (C) 2025-present redhorse26
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Source of original work: [https://github.com/Blooket-Council/Blooket-Cheats/tree/main](https://github.com/Blooket-Council/Blooket-Cheats/tree/main)
- * Source of this modified work: [https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats](https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats)
-*/
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
 
 (() => {
     let iframe = document.querySelector("iframe");
